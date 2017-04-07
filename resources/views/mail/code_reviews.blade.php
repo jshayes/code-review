6a73bbbaa19d6893bb3ac2d2b15d8749dd6dde65
Justin Hayes <justin@soapboxhq.com>
2017-04-06 22:37:41 -0400

    Updated the email template

--- resources/views/mail/code_reviews.blade.php
+++ resources/views/mail/code_reviews.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
-# Daily Code Review Report
+# Das Code Review Report
 
 @foreach($reviews as $review)
 ### Code reviews assigned to {{ $review['name'] }}
 @foreach($review['pull_requests'] as $pullRequest)
-- **{{ $pullRequest['user']['name'] }}** assigned [{{ $pullRequest['title'] }}]({{ $pullRequest['html_url'] }})
+- **{{ $pullRequest['user']['name'] }}** assigned [{{ $pullRequest['title'] }}]({{ $pullRequest['html_url'] }}) on {{ $pullRequest['head']['repo']['name'] }}
 @endforeach
 
 @endforeach
